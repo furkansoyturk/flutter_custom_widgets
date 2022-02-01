c6f12eca7ab96037aad77d668fb330b48b0b7089
furkansoyturk <furkansoyturk@icloud.com>
2022-02-01 20:32:40 +0300

    page indicator position bug fix.

--- lib/custom_widgets/page_indicator.dart
+++ lib/custom_widgets/page_indicator.dart
@@ -58,95 +58,117 @@ class _PageIndicator extends State<PageIndicator> {
     int? _itemCount = widget.itemCount!.toInt();
 
     return Container(
-      color: Colors.brown,
-      height: 200,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      // color: Colors.brown,
+      height: MediaQuery.of(context).size.height * 0.05,
+      width: MediaQuery.of(context).size.width,
+      child: Stack(
         children: [
-          IconButton(
-            onPressed: () {
-              _jumpStart();
-              setState(() {
-                _selectedIndex = 1;
-                widget.currentIndexCallBack(_selectedIndex);
-              });
-            },
-            icon:  const Icon(
-              Icons.skip_previous,
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              _moveLeft();
-              setState(() {
+              Positioned(
+                bottom: 0,
+                left: MediaQuery.of(context).size.width * 0.005,
+                child: IconButton(
+                  onPressed: () {
+                    _jumpStart();
+                    setState(() {
+                      _selectedIndex = 1;
+                      widget.currentIndexCallBack(_selectedIndex);
+                    });
+                  },
+                  icon:  const Icon(
+                    Icons.skip_previous,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: MediaQuery.of(context).size.width * 0.1,
+                child: IconButton(
+                  onPressed: () {
+                    _moveLeft();
+                    setState(() {
 
-                if( _selectedIndex > 1)
-                  {
-                    _selectedIndex = _selectedIndex - 1;
-                    widget.currentIndexCallBack(_selectedIndex);
-                  }
-              });
-            },
-            icon: const Icon(
-              Icons.keyboard_arrow_left,
-            ),
-          ),
-          SizedBox(
-            width: 175,
-            // color: Colors.amberAccent,
-            child: ListView.builder(
-                controller: _controller,
-                scrollDirection: Axis.horizontal,
-                itemExtent: 60,
-                itemCount: _itemCount,
-                itemBuilder: (context, index) {
-                  index = index+1;
-                  return ListTile(
-                    title: Text(
-                      '$index',
-                      // maxLines: 1,
-                      style: const TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    selected: index == _selectedIndex,
-                    onTap: (){
-                      setState(() {
-                        _selectedIndex = index;
-                        widget.currentIndexCallBack(_selectedIndex);
-                      });
-                    },
-                  );
-                }),
-          ),
-          IconButton(
-            onPressed: () {
-              _moveRight();
-              setState(() {
-                if(_selectedIndex < _itemCount)
-                  {
-                    _selectedIndex = _selectedIndex + 1;
-                    widget.currentIndexCallBack(_selectedIndex);
-                  }
-              });
-            },
-            icon: const Icon(
-              Icons.keyboard_arrow_right,
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              _jumpEnd();
-              setState(() {
-                _selectedIndex = _itemCount;
-                widget.currentIndexCallBack(_selectedIndex);
-              });
-            },
-            icon: const Icon(
-              Icons.skip_next,
-            ),
-          ),
+                      if( _selectedIndex > 1)
+                        {
+                          _selectedIndex = _selectedIndex - 1;
+                          widget.currentIndexCallBack(_selectedIndex);
+                        }
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.keyboard_arrow_left,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: MediaQuery.of(context).size.width * 0.275,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  child: Center(
+                    child: ListView.builder(
+                        controller: _controller,
+                        scrollDirection: Axis.horizontal,
+                        itemExtent: 60,
+                        itemCount: _itemCount,
+                        itemBuilder: (context, index) {
+                          index = index+1;
+                          return ListTile(
+                            title: Center(
+                              child: Text(
+                                '$index',
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            selected: index == _selectedIndex,
+                            onTap: (){
+                              setState(() {
+                                _selectedIndex = index;
+                                widget.currentIndexCallBack(_selectedIndex);
+                              });
+                            },
+                          );
+                        }),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: MediaQuery.of(context).size.width * 0.1,
+                child: IconButton(
+                  onPressed: () {
+                    _moveRight();
+                    setState(() {
+                      if(_selectedIndex < _itemCount)
+                        {
+                          _selectedIndex = _selectedIndex + 1;
+                          widget.currentIndexCallBack(_selectedIndex);
+                        }
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.keyboard_arrow_right,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: MediaQuery.of(context).size.width * 0.005,
+                child: IconButton(
+                  onPressed: () {
+                    _jumpEnd();
+                    setState(() {
+                      _selectedIndex = _itemCount;
+                      widget.currentIndexCallBack(_selectedIndex);
+                    });
+                  },
+                  icon: const Icon(
+                    Icons.skip_next,
+                  ),
+                ),
+              ),
         ],
       ),
     );
